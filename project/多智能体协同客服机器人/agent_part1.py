b29069c9b8da157ab9d2758b035ba18fde812af0
Sean Gilbert <85278539+zhao-cs-debug@users.noreply.github.com>
2024-05-16 16:08:38 +0800

    多智能体协同客服机器人

---
+++ project/多智能体协同客服机器人/agent_part1.py
@@ -0,0 +1,203 @@
+import os
+import uuid
+import shutil
+import getpass
+from datetime import datetime
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import END, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from create_data import create_data
+from tools.policies import lookup_policy
+from tools.flights import (
+    fetch_user_flight_information,
+    search_flights,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+)
+from tools.car_rental import (
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+)
+from tools.hotels import (
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+)
+from tools.excursions import (
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
+from tools.utils import (
+    create_tool_node_with_fallback,
+    _print_event,
+)
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+_set_env("ANTHROPIC_API_KEY")
+_set_env("TAVILY_API_KEY")
+
+# Recommended
+_set_env("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Customer Support Bot Tutorial"
+
+create_data()
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            passenger_id = config.get("passenger_id", None)
+            state = {**state, "user_info": passenger_id}
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+# Haiku is faster and cheaper, but less accurate
+# llm = ChatAnthropic(model="claude-3-haiku-20240307")
+llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
+# You could swap LLMs, though you will likely want to update the prompts when
+# doing so!
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_1_tools = [
+    TavilySearchResults(max_results=1),
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+]
+part_1_assistant_runnable = primary_assistant_prompt | llm.bind_tools(part_1_tools)
+
+# define graph
+builder = StateGraph(State)
+
+# Define nodes: these do the work
+builder.add_node("assistant", Assistant(part_1_assistant_runnable))
+builder.add_node("action", create_tool_node_with_fallback(part_1_tools))
+# Define edges: these determine how the control flow moves
+builder.set_entry_point("assistant")
+builder.add_conditional_edges(
+    "assistant",
+    tools_condition,
+    # "action" calls one of our tools. END causes the graph to terminate (and respond to the user)
+    {"action": "action", END: END},
+)
+builder.add_edge("action", "assistant")
+
+# The checkpointer lets the graph persist its state
+# this is a complete memory for the entire graph.
+memory = SqliteSaver.from_conn_string(":memory:")
+part_1_graph = builder.compile(checkpointer=memory)
+
+# from IPython.display import Image, display
+
+# try:
+#     display(Image(part_1_graph.get_graph(xray=True).draw_mermaid_png()))
+# except:
+#     # This requires some extra dependencies and is optional
+#     pass
+
+# Let's create an example conversation a user might have with the assistant
+tutorial_questions = [
+    "Hi there, what time is my flight?",
+    "Am i allowed to update my flight to something sooner? I want to leave later today.",
+    "Update my flight to sometime next week then",
+    "The next available option is great",
+    "what about lodging and transportation?",
+    "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
+    "OK could you place a reservation for your recommended hotel? It sounds nice.",
+    "yes go ahead and book anything that's moderate expense and has availability.",
+    "Now for a car, what are my options?",
+    "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
+    "Cool so now what recommendations do you have on excursions?",
+    "Are they available while I'm there?",
+    "interesting - i like the museums, what options are there? ",
+    "OK great pick one and book it for my second day there.",
+]
+
+# Update with the backup file so we can restart from the original place in each section
+shutil.copy("travel2.backup.sqlite", "travel2.sqlite")
+thread_id = str(uuid.uuid4())
+
+config = {
+    "configurable": {
+        # The passenger_id is used in our flight tools to
+        # fetch the user's flight information
+        "passenger_id": "3442 587242",
+        # Checkpoints are accessed by thread_id
+        "thread_id": thread_id,
+    }
+}
+
+_printed = set()
+for question in tutorial_questions:
+    events = part_1_graph.stream(
+        {"messages": ("user", question)}, config, stream_mode="values"
+    )
+    for event in events:
+        _print_event(event, _printed)
---
+++ project/多智能体协同客服机器人/agent_part2.py
@@ -0,0 +1,240 @@
+import os
+import uuid
+import shutil
+import getpass
+from datetime import datetime
+
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_core.messages import ToolMessage
+
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langgraph.graph import END, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+from create_data import create_data
+from tools.policies import lookup_policy
+from tools.flights import (
+    fetch_user_flight_information,
+    search_flights,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+)
+from tools.car_rental import (
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+)
+from tools.hotels import (
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+)
+from tools.excursions import (
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
+from tools.utils import (
+    create_tool_node_with_fallback,
+    _print_event,
+)
+
+def _set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+_set_env("ANTHROPIC_API_KEY")
+_set_env("TAVILY_API_KEY")
+
+# Recommended
+_set_env("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Customer Support Bot Tutorial"
+
+create_data()
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+    user_info: str
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            passenger_id = config.get("passenger_id", None)
+            result = self.runnable.invoke(state)
+            # If the LLM happens to return an empty response, we will re-prompt it
+            # for an actual response.
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
+# Haiku is faster and cheaper, but less accurate
+# llm = ChatAnthropic(model="claude-3-haiku-20240307")
+llm = ChatAnthropic(model="claude-3-sonnet-20240229", temperature=1)
+# You could also use OpenAI or another model, though you will likely have
+# to adapt the prompts
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(model="gpt-4-turbo-preview")
+
+assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now())
+
+part_2_tools = [
+    TavilySearchResults(max_results=1),
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+]
+part_2_assistant_runnable = assistant_prompt | llm.bind_tools(part_2_tools)
+
+builder = StateGraph(State)
+
+def user_info(state: State):
+    return {"user_info": fetch_user_flight_information.invoke({})}
+
+# NEW: The fetch_user_info node runs first, meaning our assistant can see the user's flight information without
+# having to take an action
+builder.add_node("fetch_user_info", user_info)
+builder.set_entry_point("fetch_user_info")
+builder.add_node("assistant", Assistant(part_2_assistant_runnable))
+builder.add_node("action", create_tool_node_with_fallback(part_2_tools))
+builder.add_edge("fetch_user_info", "assistant")
+builder.add_conditional_edges(
+    "assistant", tools_condition, {"action": "action", END: END}
+)
+builder.add_edge("action", "assistant")
+
+memory = SqliteSaver.from_conn_string(":memory:")
+part_2_graph = builder.compile(
+    checkpointer=memory,
+    # NEW: The graph will always halt before executing the "action" node.
+    # The user can approve or reject (or even alter the request) before
+    # the assistant continues
+    interrupt_before=["action"],
+)
+
+# from IPython.display import Image, display
+
+# try:
+#     display(Image(part_2_graph.get_graph(xray=True).draw_mermaid_png()))
+# except:
+#     # This requires some extra dependencies and is optional
+#     pass
+
+# Let's create an example conversation a user might have with the assistant
+tutorial_questions = [
+    "Hi there, what time is my flight?",
+    "Am i allowed to update my flight to something sooner? I want to leave later today.",
+    "Update my flight to sometime next week then",
+    "The next available option is great",
+    "what about lodging and transportation?",
+    "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
+    "OK could you place a reservation for your recommended hotel? It sounds nice.",
+    "yes go ahead and book anything that's moderate expense and has availability.",
+    "Now for a car, what are my options?",
+    "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
+    "Cool so now what recommendations do you have on excursions?",
+    "Are they available while I'm there?",
+    "interesting - i like the museums, what options are there? ",
+    "OK great pick one and book it for my second day there.",
+]
+
+# Update with the backup file so we can restart from the original place in each section
+shutil.copy("travel2.backup.sqlite", "travel2.sqlite")
+thread_id = str(uuid.uuid4())
+
+config = {
+    "configurable": {
+        # The passenger_id is used in our flight tools to
+        # fetch the user's flight information
+        "passenger_id": "3442 587242",
+        # Checkpoints are accessed by thread_id
+        "thread_id": thread_id,
+    }
+}
+
+_printed = set()
+# We can reuse the tutorial questions from part 1 to see how it does.
+for question in tutorial_questions:
+    events = part_2_graph.stream(
+        {"messages": ("user", question)}, config, stream_mode="values"
+    )
+    for event in events:
+        _print_event(event, _printed)
+    snapshot = part_2_graph.get_state(config)
+    while snapshot.next:
+        # We have an interrupt! The agent is trying to use a tool, and the user can approve or deny it
+        # Note: This code is all outside of your graph. Typically, you would stream the output to a UI.
+        # Then, you would have the frontend trigger a new run via an API call when the user has provided input.
+        user_input = input(
+            "Do you approve of the above actions? Type 'y' to continue;"
+            " otherwise, explain your requested changed.\n\n"
+        )
+        if user_input.strip() == "y":
+            # Just continue
+            result = part_2_graph.invoke(
+                None,
+                config,
+            )
+        else:
+            # Satisfy the tool invocation by
+            # providing instructions on the requested changes / change of mind
+            result = part_2_graph.invoke(
+                {
+                    "messages": [
+                        ToolMessage(
+                            tool_call_id=event["messages"][-1].tool_calls[0]["id"],
+                            content=f"API call denied by user. Reasoning: '{user_input}'. Continue assisting, accounting for the user's input.",
+                        )
+                    ]
+                },
+                config,
+            )
+        snapshot = part_2_graph.get_state(config)
